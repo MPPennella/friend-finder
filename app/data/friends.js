9485798d231789f8e6b727e9b9ea1022f68d9c2f
M. Pennella <michael.pennella@gmail.com>
2018-11-08 15:55:53 -0800

    Added template for friends array and exported from friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+// Exports initial list of friend data
+module.exports = [
+    {
+        name: "Test",
+        photo: "test.jpg",
+        scores: [
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3
+        ]
+    }
+]
--- server.js
+++ server.js
@@ -1,18 +1,23 @@
+// Import required modules
 const path = require("path")
 const express = require("express")
 
+// Configure Express app
 var app = express()
-
-const PORT = process.env.PORT || 8080
-
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Define server listening port
+const PORT = process.env.PORT || 8080
 
 // Import routes for API and HTML pages
 require("./app/routing/apiRoutes")(app)
 require("./app/routing/htmlRoutes")(app)
 
+// Import friend data
+let friends = require("./app/data/friends")
+console.log(friends)
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
