2760ddb9b1f1c6cf06c36f7c581563fbc2878b6f
M. Pennella <michael.pennella@gmail.com>
2018-11-09 02:51:33 -0800

    Added friend-matching logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -14,11 +14,41 @@ module.exports = function(app) {
 
         // Make sure new friend has correct keys
         if (newFriend.name && newFriend.photo && newFriend.scores && newFriend.scores.length == 10) {
+
+            // Find best match to newFriend
+            // Least difference in scores is best match
+            
+            // Get array of difference scores
+            let differences = friends.map( (friend) => {
+                console.log(friend.name)
+
+                // Calculate amount of difference
+                let diff = 0
+                for (let i=0; i<friend.scores.length; i++) {
+                    // Add absolute value of difference in rating 
+                    diff += Math.abs(friend.scores[i]-newFriend.scores[i])
+                    console.log(diff)
+                }
+                return diff
+            })
+
+            // Find lowest difference
+            let bestMatchIndex=0
+            for(let i=0; i<differences.length; i++) {
+                if (differences[i] < differences[bestMatchIndex]) {
+                    bestMatchIndex = i
+                }
+            }
+
+            let bestMatch = friends[bestMatchIndex]
+
+            console.log("BEST MATCH: ", bestMatch.name)
+            console.log("SCORE: ",differences[bestMatchIndex])
             // Add new entry to friends array
             friends.push(newFriend)
 
-            // Return newFriend object as response
-            res.send(newFriend)
+            // Return bestMatch friend object as response
+            res.send(bestMatch)
         } else {
             // Send bad request error
             res.status(400).send('Bad Request')
