afa8d2e206fae6b95ea63419510fc3e0005be3b7
M. Pennella <michael.pennella@gmail.com>
2018-11-08 16:29:08 -0800

    Added handling for POST route at /api/friends

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,6 +7,21 @@ module.exports = function(app) {
     })
     
     app.post("/api/friends", (req, res) => {
-        res.end()
+        console.log("POST request received")
+        
+        let newFriend = req.body
+        console.log(newFriend)
+
+        // Make sure new friend has correct keys
+        if (newFriend.name && newFriend.photo && newFriend.scores && newFriend.scores.length == 10) {
+            // Add new entry to friends array
+            friends.push(newFriend)
+
+            // Return newFriend object as response
+            res.send(newFriend)
+        } else {
+            // Send bad request error
+            res.status(400).send('Bad Request')
+        }
     })
 }
