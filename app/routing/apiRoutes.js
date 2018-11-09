51b73905468819e6e2ac0d8359655a56fb09608e
M. Pennella <michael.pennella@gmail.com>
2018-11-08 16:07:02 -0800

    Added handling for API GET request  to /api/friends

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,8 +1,9 @@
 const path = require("path")
+let friends = require("../data/friends")
 
 module.exports = function(app) {
     app.get("/api/friends", (req, res) => {
-        res.end()
+        res.send(friends)
     })
     
     app.post("/api/friends", (req, res) => {
