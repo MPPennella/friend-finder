050fb4df335170170d17dd90f6fcea3c1044d6c3
M. Pennella <michael.pennella@gmail.com>
2018-11-09 02:27:13 -0800

    Added more default friends to list

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,19 +1,67 @@
 // Exports initial list of friend data
 module.exports = [
     {
-        name: "Test",
+        name: "Angela",
+        photo: "test.jpg",
+        scores: [
+            2,
+            4,
+            4,
+            1,
+            2,
+            3,
+            2,
+            3,
+            4,
+            3
+        ]
+    },
+    {
+        name: "Bill",
+        photo: "test.jpg",
+        scores: [
+            4,
+            5,
+            5,
+            4,
+            3,
+            4,
+            3,
+            1,
+            2,
+            1
+        ]
+    },
+    {
+        name: "Chad",
         photo: "test.jpg",
         scores: [
             3,
+            2,
+            2,
+            5,
+            4,
+            2,
+            1,
+            5,
             3,
+            1
+        ]
+    },
+    {
+        name: "Daniel",
+        photo: "test.jpg",
+        scores: [
             3,
+            4,
+            4,
+            2,
             3,
+            5,
+            5,
+            1,
             3,
-            3,
-            3,
-            3,
-            3,
-            3
+            4
         ]
     }
 ]
